96e713de6aa74f3afdc929016c8123737c7ec3b6
scheck93 <109469032+scheck93@users.noreply.github.com>
2024-07-23 01:14:54 +0000

    updated for bottom and top suits to be the same

--- src/app.js
+++ src/app.js
@@ -6,47 +6,33 @@ window.onload = function() {
   //write your code here
   console.log("Hello World!");
 };
+
 const suitsArray = ["♠", "♦", "♥", "♣"];
 
-function randomGenerator1() {
+function randomGenerator() {
   const randomIndex = Math.floor(Math.random() * suitsArray.length);
   return suitsArray[randomIndex];
 }
 
-const selectedItem1 = randomGenerator1();
-document.getElementById("topLeftIcon").innerText = selectedItem1;
+const selectedItem = randomGenerator();
+document.getElementById("topLeftIcon").innerText = selectedItem;
+document.getElementById("bottomRightIcon").innerText = selectedItem;
 
 // Set the color based on the suit
-if (selectedItem1 === "♦" || selectedItem1 === "♥") {
-  topLeftIcon.style.color = "red";
+if (selectedItem === "♦" || selectedItem === "♥") {
+  document.getElementById("topLeftIcon").style.color = "red";
+  document.getElementById("bottomRightIcon").style.color = "red";
 } else {
-  topLeftIcon.style.color = "black";
+  document.getElementById("topLeftIcon").style.color = "black";
+  document.getElementById("bottomRightIcon").style.color = "black";
 }
-
-const suitsArray2 = ["♠", "♦", "♥", "♣"];
-
-function randomGenerator2() {
-  const randomIndex = Math.floor(Math.random() * suitsArray.length);
-  return suitsArray2[randomIndex];
-}
-
-const selectedItem2 = randomGenerator2();
-document.getElementById("bottomRightIcon").innerText = selectedItem2;
-
-if (selectedItem2 === "♦" || selectedItem2 === "♥") {
-  bottomRightIcon.style.color = "red";
-} else {
-  bottomRightIcon.style.color = "black";
-}
-
-// number and Queen or King generator
 
 const numberArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Q", "K"];
 
-function randomGenerator3() {
+function randomGeneratorNumber() {
   const randomIndex = Math.floor(Math.random() * numberArray.length);
   return numberArray[randomIndex];
 }
 
-const middleNumber = randomGenerator3();
+const middleNumber = randomGeneratorNumber();
 document.getElementById("middleNumber").innerText = middleNumber;
